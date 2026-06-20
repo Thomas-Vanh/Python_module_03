379719f42841214efd66fa71821f39bfa9499ca5
Thomas Vanhaudenard <tvanhaud@fu-r9-p5.s19.be>
2026-06-20 18:02:02 +0200

    draft exo 3

--- ex2/ft_coordinate_system.py
+++ ex2/ft_coordinate_system.py
@@ -4,3 +4,13 @@ import math
 
 def get_player_pos():
     new_pos = input("Enter new coordinates as floats in format 'x,y,z':")
+    i = 0
+    while i < 4:
+        try:
+            float(new_pos)
+        except ValueError:
+            print(f"Invalid Syntax")
+
+
+if __name__ == "__main__":
+    get_player_pos()
